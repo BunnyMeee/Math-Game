5cc63ee311a73ed43794b7e7f6f7b4052b7e2c60
BunnyMeee <75510733+BunnyMeee@users.noreply.github.com>
2021-09-03 15:43:51 -0600

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -10,3 +10,25 @@ check_button = "<br><br><button class='btn btn-info' onclick='check()'>check</bu
 row = question_number + input_box + check_button;
 document.getElementById("number1").value = "";
 document.getElementById("number2").value = "";
+
+function check() {
+    get_answer = document.getElementById("player1_score").value;
+    if (answer_turn == "player1") {
+        update_player1_score - player1_score + 1;
+        document.getElementById("player1_score").innerHTML = update_player1_score;
+
+    }
+    else {
+        update_player2_score - player2_score + 1;
+        document.getElementById("player2_score").innerHTML = update_player1_score;
+    }
+
+    if (question_turn == "player1") {
+        question_turn = "player2";
+        document.getElementById("player_question").innerHTML = "question turn - " + player2_name;
+    }
+    else {
+        question_turn = "player1";
+        document.getElementById("player_question").innerHTML = "question turn - " + player1_name;
+    }
+}
